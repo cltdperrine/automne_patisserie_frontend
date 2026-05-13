7dc901847673643c6667513d831722269c1f75d4
perrinecltd <cltd.perrine@gmail.com>
2026-05-13 10:22:14 +0200

    feat: improve login and register pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,14 +5,16 @@ export default function Login() {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
-
-    error: null,
   });
+
+  const [error, setError] = useState("");
 
   function handleChange(event) {
     const { name, value } = event.target;
 
     setFormData((state) => ({ ...state, [name]: value }));
+
+    setError("");
   }
 
   async function handleSubmit() {
@@ -22,15 +24,22 @@ export default function Login() {
         formData,
       );
       console.log(response.data);
+<<<<<<< Updated upstream
     } catch {
       setFormData((state) => ({
         ...state,
         error: "E-mail ou mot de passe incorrects",
       }));
+=======
+    } catch (error) {
+      console.log(error);
+      setError("Email ou mot de passe incorrects");
+>>>>>>> Stashed changes
     }
   }
 
   return (
+<<<<<<< Updated upstream
     <div className="min-h-screen bg-white flex flex-col items-center px-4 py-16">
       <div className="flex gap-6 mb-12">
         <span className="text-2xl font-semibold text-[#9f9f9f]">
@@ -81,5 +90,66 @@ export default function Login() {
         </button>
       </form>
     </div>
+=======
+    <>
+      <Header />
+      <div className="min-h-screen bg-white flex flex-col items-center px-4 py-16">
+        <div className="flex gap-6 mb-12">
+          <span className="text-2xl font-semibold text-[#9f9f9f]">
+            Se connecter
+          </span>
+          <Link
+            to="/register"
+            className="text-2xl font-semibold text-black cursor-pointer"
+          >
+            Créer un compte
+          </Link>
+        </div>
+
+        <div>{formData.error}</div>
+        {/* Input adresse email */}
+        <form className="w-full max-w-lg bg-white border border-gray-100 rounded-xl shadow-sm p-8 flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <label className="text-base font-medium text-black">
+              Adresse e-mail*
+            </label>
+
+            <input
+              name="email"
+              onChange={handleChange}
+              value={formData.email}
+              type="email"
+              placeholder="abc@exemple.com"
+              className="w-full h-[52px] rounded-[10px] border border-[#9f9f9f] px-4 text-base outline-none focus:border-[#b58275] transition-colors"
+            />
+          </div>
+          {/* Input mdp */}
+          <div className="flex flex-col gap-2">
+            <label className="text-base font-medium text-black">
+              Mot de passe*
+            </label>
+
+            <input
+              name="password"
+              onChange={handleChange}
+              value={formData.password}
+              type="password"
+              className="w-full h-[52px] rounded-[10px] border border-[#9f9f9f] px-4 text-base outline-none focus:border-[#b58275] transition-colors"
+            />
+          </div>
+          {error && <p className="text-red-500 text-sm">{error}</p>}
+          {/* button pour se connecter */}
+          <button
+            onClick={handleSubmit}
+            type="button"
+            className="w-full h-[55px] rounded-[5px] bg-[#b58275] text-white text-base font-medium hover:opacity-90 transition-opacity cursor-pointer"
+          >
+            Se connecter
+          </button>
+        </form>
+      </div>
+      <Footer />
+    </>
+>>>>>>> Stashed changes
   );
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,11 @@
 import { useState } from "react";
+<<<<<<< Updated upstream
+=======
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
+import axios from "axios";
+>>>>>>> Stashed changes
 
 export default function Register() {
   const [formData, setFormData] = useState({
@@ -6,6 +13,8 @@ export default function Register() {
     password: "",
     confirmPassword: "",
   });
+
+  const [error, setError] = useState("");
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -16,17 +25,26 @@ export default function Register() {
     }));
   }
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
 
-    if (formData.confirmPassword === formData.password) {
-      console.log("Compte crée");
-    } else {
-      console.error("Les mots de passe ne correspondent pas");
+    if (formData.password !== formData.confirmPassword) {
+      setError("Les mots de passe ne correspondent pas");
+      return;
+    }
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/auth/register",
+        formData,
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
     }
   }
 
   return (
+<<<<<<< Updated upstream
     <div className="min-h-screen bg-white flex flex-col items-center px-4 py-16">
       <div className="flex gap-6 mb-12">
         <span className="text-2xl font-semibold text-left text-[#9f9f9f] cursor-pointer">
@@ -35,6 +53,76 @@ export default function Register() {
         <span className="text-2xl font-semibold text-black">
           Créer un compte
         </span>
+=======
+    <>
+      <Header />
+
+      <div className="min-h-screen bg-white flex flex-col items-center px-4 py-16">
+        <div className="flex gap-6 mb-12">
+          <Link
+            to="/login"
+            className="text-2xl font-semibold text-left text-black cursor-pointer"
+          >
+            Se connecter
+          </Link>
+          <span className="text-2xl font-semibold text-[#9f9f9f]">
+            Créer un compte
+          </span>
+        </div>
+        {/* Input adresse email */}
+        <div className="w-full max-w-lg bg-white border border-gray-100 rounded-xl shadow-sm p-8 flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <label className="text-base font-medium text-black">
+              Votre e-mail*
+            </label>
+            <input
+              name="email"
+              value={formData.email}
+              type="email"
+              onChange={handleChange}
+              className="w-full h-[52px] rounded-[10px] border border-[#9f9f9f] px-4 text-base outline-none focus:border-[#b58275] transition-colors"
+              placeholder="abc@exemple.com"
+            />
+          </div>
+          {/* Input mdp */}
+          <div className="flex flex-col gap-2">
+            <label className="text-base font-medium text-black">
+              Mot de passe*
+            </label>
+            <input
+              name="password"
+              value={formData.password}
+              type="password"
+              onChange={handleChange}
+              className="w-full h-[52px] rounded-[10px] border border-[#9f9f9f] px-4 text-base outline-none focus:border-[#b58275] transition-colors"
+            />
+          </div>
+
+          {/* confirmation mdp */}
+          <div className="flex flex-col gap-2">
+            <label className="text-base font-medium text-black">
+              Confirmation du mot de passe*
+            </label>
+
+            <input
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              type="password"
+              onChange={handleChange}
+              className="w-full h-[52px] rounded-[10px] border border-[#9f9f9f] px-4 text-base outline-none focus:border-[#b58275] transition-colors"
+            />
+          </div>
+          {error && <p className="text-red-500 text-sm">{error}</p>}
+          {/* button pour s'inscrire' */}
+          <button
+            onClick={handleSubmit}
+            type="button"
+            className="w-full h-[55px] rounded-[5px] bg-[#b58275] text-white text-base font-medium hover:opacity-90 transition-opacity cursor-pointer"
+          >
+            Créer un compte
+          </button>
+        </div>
+>>>>>>> Stashed changes
       </div>
       {/* Input adresse email */}
       <form
