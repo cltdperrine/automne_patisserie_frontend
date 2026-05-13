4e5c4cdc8c20f69c271027a875917e5872e23fe0
perrinecltd <cltd.perrine@gmail.com>
2026-05-13 15:32:57 +0200

    feat: improve login and register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,7 @@
 import { useState } from "react";
-<<<<<<< Updated upstream
-=======
 import Header from "../components/Header";
 import { Link } from "react-router-dom";
-import Footer from "../components/Footer";
 import axios from "axios";
->>>>>>> Stashed changes
 
 export default function Register() {
   const [formData, setFormData] = useState({
@@ -44,16 +40,6 @@ export default function Register() {
   }
 
   return (
-<<<<<<< Updated upstream
-    <div className="min-h-screen bg-white flex flex-col items-center px-4 py-16">
-      <div className="flex gap-6 mb-12">
-        <span className="text-2xl font-semibold text-left text-[#9f9f9f] cursor-pointer">
-          Se connecter
-        </span>
-        <span className="text-2xl font-semibold text-black">
-          Créer un compte
-        </span>
-=======
     <>
       <Header />
 
@@ -122,62 +108,7 @@ export default function Register() {
             Créer un compte
           </button>
         </div>
->>>>>>> Stashed changes
       </div>
-      {/* Input adresse email */}
-      <form
-        onSubmit={handleSubmit}
-        className="w-full max-w-lg bg-white border border-gray-100 rounded-xl shadow-sm p-8 flex flex-col gap-4"
-      >
-        <div className="flex flex-col gap-2">
-          <label className="text-base font-medium text-black">
-            Votre e-mail*
-          </label>
-          <input
-            name="email"
-            value={formData.email}
-            type="email"
-            onChange={handleChange}
-            className="w-full h-[52px] rounded-[10px] border border-[#9f9f9f] px-4 text-base outline-none focus:border-[#b58275] transition-colors"
-            placeholder="abc@exemple.com"
-          />
-        </div>
-        {/* Input mdp */}
-        <div className="flex flex-col gap-2">
-          <label className="text-base font-medium text-black">
-            Mot de passe*
-          </label>
-          <input
-            name="password"
-            value={formData.password}
-            type="password"
-            onChange={handleChange}
-            className="w-full h-[52px] rounded-[10px] border border-[#9f9f9f] px-4 text-base outline-none focus:border-[#b58275] transition-colors"
-          />
-        </div>
-
-        {/* confirmation mdp */}
-        <div className="flex flex-col gap-2">
-          <label className="text-base font-medium text-black">
-            Confirmation du mot de passe*
-          </label>
-
-          <input
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            type="password"
-            onChange={handleChange}
-            className="w-full h-[52px] rounded-[10px] border border-[#9f9f9f] px-4 text-base outline-none focus:border-[#b58275] transition-colors"
-          />
-        </div>
-        {/* button pour s'inscrire' */}
-        <button
-          type="submit"
-          className="w-full h-[55px] rounded-[5px] bg-[#b58275] text-white text-base font-medium hover:opacity-90 transition-opacity cursor-pointer"
-        >
-          Créer un compte
-        </button>
-      </form>
-    </div>
+    </>
   );
 }
