0280414d60df496eb0c185216aae1b98698e0548
perrinecltd <cltd.perrine@gmail.com>
2026-06-07 15:57:46 +0200

    fix: update cors config

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+console.log("API URL :", import.meta.env.VITE_API_URL);
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + "/api",
+  baseURL: "https://automnepatisseriebackend.vercel.app/api",
   withCredentials: true,
 });
 
